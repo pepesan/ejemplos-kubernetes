05b479c83a486e62294de77c2225fb0f0419d1b9
pepesan <pepesan@gmail.com>
2025-12-06 09:50:29 -0400

    mejorados ejemplo de canary

--- src/09_canary_rollout_a_b/10_send_request.sh
+++ src/09_canary_rollout_a_b/10_send_request.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SERVER_NAME=echo.prod.mydomain.com
 SERVER_IP=192.168.49.2
-# lanza consulta
-for i in $(seq 1 10); do curl -s --resolve $SERVER_NAME:80:$SERVER_IP echo.prod.mydomain.com  | grep "Hostname"; done
 
+for i in $(seq 1 10); do
+  curl -s --resolve "$SERVER_NAME:80:$SERVER_IP" "http://$SERVER_NAME/" | grep "Hostname"
+done
